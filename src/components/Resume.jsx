bcc7bcd515ce8f3da94cb84fae615fee7672df8b
Bradley Troll <bradleytroll@gmail.com>
2024-05-09 17:45:44 -0400

    Completes Resume.jsx

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,13 +4,31 @@ const Resume = () => {
     return (
         <section>
             <h2>Resume</h2>
-            <a href='path-to-resume.pdf' download>Download Resume</a>
-            <ul>
-                <li>Proficiency 1</li>
-                <li>Proficiency 2</li>
+            <a href='/bradleytroll.pdf' download="Bradley_Troll_Resume">Click Here to Download Resume</a>
+            
+            <h3 style={{ fontWeight: 'bold', fontSize: '1.2em', textAlign: 'center', marginTop: '20px' }}>Front End</h3>
+            <ul className="centered-list">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>responsive design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
             </ul>
+
+            <h3 style={{ fontWeight: 'bold', fontSize: '1.2em', textAlign: 'center', marginTop: '20px' }}>Back End</h3>
+            <ul className="centered-list">
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+            </ul>
+            <br></br>
         </section>
-    )
+    );
 }
 
 export default Resume;
