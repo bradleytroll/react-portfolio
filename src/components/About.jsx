9fe7d03ec30c817c549d2e0cd6183b61cbf76ca8
Bradley Troll <bradleytroll@gmail.com>
2024-05-09 09:20:27 -0400

    Adds About.jsx and Header.jsx file

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <img src="https://pbs.twimg.com/media/DyK8fQpW0Aky8G3.jpg" alt="Bradley Troll" />
+            <p>Bio</p>
+        </div>
+    )
+}
+
+export default About;
