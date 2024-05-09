88d2b16f20d09836cf891ae0c87f10778ffebf40
Bradley Troll <bradleytroll@gmail.com>
2024-05-09 11:27:33 -0400

    Comletes portfolio, reruns npm run build

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,6 +3,8 @@ import funCastImage from '../assets/funcast.png'
 import buffyImage from '../assets/buffy.png'
 import fiveDayForecasterImage from '../assets/five-day-forecaster.png'
 import workDayScheduler from '../assets/work-day-scheduler.png'
+import passwordGenerator from '../assets/password-generator.png'
+import shelfLife from '../assets/shelflife.png'
 
 const Portfolio = () => {
     const projects = [
@@ -10,6 +12,9 @@ const Portfolio = () => {
         { title: "Buffy Trivia Challenge", imageUrl: buffyImage, liveUrl: "https://bradleytroll.github.io/buffy-trivia-challenge/", repoUrl: "https://github.com/bradleytroll/buffy-trivia-challenge" },
         { title: "Five-Day Forecaster", imageUrl: fiveDayForecasterImage, liveUrl: "https://bradleytroll.github.io/five-day-forecaster/", repoUrl: "https://github.com/bradleytroll/five-day-forecaster" },
         { title: "Work Day Scheduler", imageUrl: workDayScheduler, liveUrl: "https://bradleytroll.github.io/five-day-forecaster/", repoUrl: "https://github.com/bradleytroll/work-day-scheduler" },
+        { title: "ShelfLife", imageUrl: shelfLife, liveUrl: "https://shelflife-91f231d5543b.herokuapp.com/", repoUrl: "https://github.com/bradleytroll/round2" },
+        { title: "Password Generator", imageUrl: passwordGenerator, liveUrl: "https://bradleytroll.github.io/password-generator/", repoUrl: "https://github.com/bradleytroll/password-generator" },
+
     ];
 
     return (
@@ -35,3 +40,4 @@ const Portfolio = () => {
 }
 
 export default Portfolio
+
