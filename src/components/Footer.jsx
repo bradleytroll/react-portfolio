dbd2bfff4276348c8897c2607b208a6b1dc1bc67
Bradley Troll <bradleytroll@gmail.com>
2024-05-09 17:18:54 -0400

    Adds logos to footer links via FontAwesomeIcon. Adjusts styling somewhat.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
@@ -6,11 +8,17 @@ const Footer = () => {
             <br></br>
             Connect with me: 
             <span style={{ margin: '0 10px' }}></span>
-            <a href="https://github.com/bradleytroll" target="_blank" rel="noopener noreferrer">Github</a>
+            <a href="https://github.com/bradleytroll" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+                </a>
             <span style={{ margin: '0 10px' }}></span>
-            <a href="https://www.linkedin.com/in/bradleytroll/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/bradleytroll/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                </a>
             <span style={{ margin: '0 10px' }}></span>
-            <a href="https://twitter.com/bradleytroll" target="_blank" rel="noopener noreferrer">Twitter</a>
+            <a href="https://twitter.com/bradleytroll" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+                </a>
             <br></br>
             <br></br>
         </footer>
