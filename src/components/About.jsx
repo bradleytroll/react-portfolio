01d845d55437f4385491e86b9d2cfc1b52c6250d
Bradley Troll <bradleytroll@gmail.com>
2024-05-09 13:10:31 -0400

    Adds bio info and some padding for the paragraph element

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,13 @@ const About = () => {
                     borderRadius: "20px"  
                 }}
             />
-            <p>Bio</p>
+            <h1>Bio</h1>
+            <p style={{
+                marginLeft: '40px',  // Adds space to the left
+                marginRight: '40px'  // Adds space to the right
+            }}>
+                After 15 years of enriching the minds of high school and college students as an English teacher, I am embarking on a new journey into the realm of full-stack web development. My decision to transition stems from a desire to explore new opportunities and challenges in the rapidly evolving tech industry. Throughout my teaching career, I honed key skills such as organization, communication, collaboration, and problem-solving, which are just as essential in the world of web development. As I pivot to this exciting new field, I am eager to blend my educational expertise with my growing technical skills to contribute to innovative projects and solutions.
+            </p>
         </section>
     )
 }
